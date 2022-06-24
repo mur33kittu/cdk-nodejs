733b06cc23d474d700738e7ee794cae9ce5af8c0
Murthy Guruswamy <mur33kittu@gmail.com>
2022-06-23 22:08:06 -0400

    initial commit

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -6,7 +6,7 @@ import { copySync } from 'fs-extra';
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id + "Wrapper");
 
     const api = new apigateway.RestApi(this, 'api', {
       description: 'newsApi',
@@ -28,17 +28,20 @@ export class CdkAppStack extends cdk.Stack {
     });
 
     const packages = ["asynckit", "axios", "combined-stream", "delayed-stream", "follow-stream", "form-data", "mime-db", "mime-types", "node-gyp-build"];
-    const nodeModulesPath = join(__dirname, "node_modules");
+    const nodeModulesPath = join(__dirname, "../node_modules");
     const buildPath = join(__dirname, "../dist");
-    copySync(join(nodeModulesPath, ),
-      join(buildPath, "nodejs"))
+    for (const packageName of packages) {
+      copySync(join(nodeModulesPath, packageName),
+        join(buildPath, id, "nodejs", "node_modules", packageName))
+    }
 
-    const myLayers = new lambda.LayerVersion(this, 'calc-layer', {
+
+    const myLayers = new lambda.LayerVersion(this, 'MyLayers', {
       compatibleRuntimes: [
-        lambda.Runtime.NODEJS_12_X,
+        lambda.Runtime.NODEJS_16_X,
         lambda.Runtime.NODEJS_14_X,
       ],
-      code: lambda.Code.fromAsset(join(__dirname, "CdkAppStack")),
+      code: lambda.Code.fromAsset(join(__dirname, "../dist/CdkAppStack")),
       description: 'lambda layer',
     });
 
